265909051ca9607c166c67fe59b6ca95112492eb
AntonG <10020anton99@gmail.com>
2025-12-11 14:36:00 +0000

    C/+myCi++

---
+++ MyProgram/C/myCi/test/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Привет, мир! Hello from C!\n");
+    printf("Это моя первая программа на C в Linux\n");
+    
+    int number = 42;
+    printf("Мое любимое число: %d\n", number);
+    
+    return 0;
+}
