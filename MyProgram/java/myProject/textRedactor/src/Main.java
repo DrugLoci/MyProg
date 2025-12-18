8e0c353cda3be4c8523419a3dcf08eb26f87ef2f
AntonG <10020anton99@gmail.com>
2025-12-18 10:59:01 +0000

    myProdject++

---
+++ MyProgram/java/myProject/textRedactor/src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Text Redactor ot GoroHovSoft");
+        frame.setBounds(100, 100, 500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton button = new JButton();
+        button.setText("Click me!");
+        
+
+        frame.setVisible(true);
+    }
+}
---
+++ MyProgram/java/myTestJavaFunc/GuiSwing.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class GuiSwing {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Тестовый редактор от GoroHova");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(100, 100, 300, 300);
+        frame.setVisible(true);
+    }
+}
