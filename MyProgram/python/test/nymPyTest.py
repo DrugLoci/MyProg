ed3304d091b3c4c7957b8f9f5ccfc3446c43550a
Anton <10020anton@gmail.com>
2026-01-30 16:42:07 +0300

    python/test++

---
+++ MyProgram/python/test/nymPyTest.py
@@ -0,0 +1,38 @@
+import numpy
+
+# arrey = numpy.array([1,2,3,4,5])
+# arrey2 = numpy.array((1,2,3,4,5))
+# arrey3 = [1,2,3,4,5]
+#
+# print(f"""{arrey}
+# {arrey2}
+# {arrey3}""")
+
+# array = numpy.zeros((10,10))
+# print(array)
+
+# array = numpy.ones((10,10))
+# print(array)
+
+# array = numpy.empty((2,2))
+# print(array)
+
+# array = numpy.arange(10,100,10)
+# print(array)
+
+# array = numpy.linspace(0,10)
+# print(array)
+
+# array = numpy.full((10,10,10),5)
+# print(array)
+# print(array.ndim) # покажет сколько из измерений в тензоре
+
+# random_array = numpy.random.rand(2,2)
+# print(random_array)
+
+# array_random = numpy.random.randint(1,10, size=(3,3))
+# print(array_random)
+
+# random_array = numpy.random.rand(3,3)
+# print(f"""Ndim = {random_array.ndim}
+# Share = {random_array.shape}""")
